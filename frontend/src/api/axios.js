e14d85d3f5e21a6b5c50e86d863ec675383f505d
varshini <varshinimoorthi20@gmail.com>
2026-06-14 00:11:55 +0530

    connect deployed backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://linkforge-production-2973.up.railway.app/api",
 });
 
-export default API;
+export default API;zz
